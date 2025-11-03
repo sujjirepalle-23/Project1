5fc0958bac09c17d3a51bba45998abb9ffe88ee3
sujitha <surepalle05@gmail.com>
2025-11-03 18:55:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const buttons = document.querySelectorAll('button');
+
+const resultE1 = document.getElementById("result");
+
+const playerScoreE1=document.getElementById("user-score");
+const computerScoreE1=document.getElementById("computer-score");
+
+
+let playerScore = 0;
+let computerScore =0 ;
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const result = playRound(button.id, computerPlay());
+      resultE1.textContent = result;
+    });
+})
+
+function computerPlay() {
+    const choices = ["rock", "paper", "'scissor"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "it's tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") || 
+        (playerSelection === "paper" && computerSelection === "rock") || 
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ){
+        playerScore++;
+        playerScoreE1.textContent = playerScore;
+        return "you win! " + playerSelection + " beates " + computerSelection;
+    }else{
+        computerScore++;
+        computerScoreE1.textContent = computerScore;
+        return "you lose! " + computerSelection + " beates " + playerSelection;
+    }
+
+}
